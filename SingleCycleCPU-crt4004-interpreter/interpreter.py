36544cf2cb7ae4fbc8dcc49012e737141eabec8e
yummycarrot <yummycarrot@qq.com>
2019-11-22 21:54:03 +0800

    update interpreter

--- SingleCycleCPU-crt4004-interpreter/interpreter.py
+++ SingleCycleCPU-crt4004-interpreter/interpreter.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 
 # interpreter for crt4004 ROM
-# version 1.0 beta
+# version 1.0
+
+import sys
 
 # OP
 OP_RTYPE = '000000'
@@ -19,8 +21,8 @@ R_OR = '100101'
 R_SLT = '101010'
 
 # DEFINE
-_FORMAT_WORD_TO_BYTE_ = False
-_FORMAT_BIN_TO_HEX_ = True
+_FORMAT_WORD_TO_BYTE_ = True
+_FORMAT_BIN_TO_HEX_ = False
 _FORMAT_HEX_UPPERCASE_ = True
 
 
@@ -35,46 +37,46 @@ def dec_str_to_bin(dec_str, bin_len):
 
 
 # ASM to BIN
-def asm_interpreter(instr_asm):
-    instr_bin = ""
-    if instr_asm[0] == 'PC':
-        instr_bin = '@' + dec_str_to_hex(instr_asm[1])
-    elif instr_asm[0] == 'ADD':
-        instr_bin = (OP_RTYPE
-                     + dec_str_to_bin(instr_asm[1].replace('$', ''), 5)
-                     + dec_str_to_bin(instr_asm[2].replace('$', ''), 5)
-                     + dec_str_to_bin(instr_asm[3].replace('$', ''), 5)
+def asm_interpreter(asm_instr):
+    bin_instr = ""
+    if asm_instr[0] == 'PC':
+        bin_instr = '@' + dec_str_to_hex(asm_instr[1])
+    elif asm_instr[0] == 'ADD':
+        bin_instr = (OP_RTYPE
+                     + dec_str_to_bin(asm_instr[1].replace('$', ''), 5)
+                     + dec_str_to_bin(asm_instr[2].replace('$', ''), 5)
+                     + dec_str_to_bin(asm_instr[3].replace('$', ''), 5)
                      + '00000' + R_ADD)
-    elif instr_asm[0] == 'SUB':
-        instr_bin = (OP_RTYPE
-                     + dec_str_to_bin(instr_asm[1].replace('$', ''), 5)
-                     + dec_str_to_bin(instr_asm[2].replace('$', ''), 5)
-                     + dec_str_to_bin(instr_asm[3].replace('$', ''), 5)
+    elif asm_instr[0] == 'SUB':
+        bin_instr = (OP_RTYPE
+                     + dec_str_to_bin(asm_instr[1].replace('$', ''), 5)
+                     + dec_str_to_bin(asm_instr[2].replace('$', ''), 5)
+                     + dec_str_to_bin(asm_instr[3].replace('$', ''), 5)
                      + '00000' + R_SUB)
-    elif instr_asm[0] == 'LW':
-        instr_bin = (OP_LW
-                     + dec_str_to_bin(instr_asm[1].replace('$', ''), 5)
-                     + dec_str_to_bin(instr_asm[2].replace('$', ''), 5)
-                     + dec_str_to_bin(instr_asm[3], 16))
-    elif instr_asm[0] == 'SW':
-        instr_bin = (OP_SW
-                     + dec_str_to_bin(instr_asm[1].replace('$', ''), 5)
-                     + dec_str_to_bin(instr_asm[2].replace('$', ''), 5)
-                     + dec_str_to_bin(instr_asm[3], 16))
-    elif instr_asm[0] == 'BEQ':
-        instr_bin = (OP_BEQ
-                     + dec_str_to_bin(instr_asm[1].replace('$', ''), 5)
-                     + dec_str_to_bin(instr_asm[2].replace('$', ''), 5)
-                     + dec_str_to_bin(instr_asm[3], 16))
-    elif instr_asm[0] == 'ADDI':
-        instr_bin = (OP_ADDI
-                     + dec_str_to_bin(instr_asm[1].replace('$', ''), 5)
-                     + dec_str_to_bin(instr_asm[2].replace('$', ''), 5)
-                     + dec_str_to_bin(instr_asm[3], 16))
+    elif asm_instr[0] == 'LW':
+        bin_instr = (OP_LW
+                     + dec_str_to_bin(asm_instr[1].replace('$', ''), 5)
+                     + dec_str_to_bin(asm_instr[2].replace('$', ''), 5)
+                     + dec_str_to_bin(asm_instr[3], 16))
+    elif asm_instr[0] == 'SW':
+        bin_instr = (OP_SW
+                     + dec_str_to_bin(asm_instr[1].replace('$', ''), 5)
+                     + dec_str_to_bin(asm_instr[2].replace('$', ''), 5)
+                     + dec_str_to_bin(asm_instr[3], 16))
+    elif asm_instr[0] == 'BEQ':
+        bin_instr = (OP_BEQ
+                     + dec_str_to_bin(asm_instr[1].replace('$', ''), 5)
+                     + dec_str_to_bin(asm_instr[2].replace('$', ''), 5)
+                     + dec_str_to_bin(asm_instr[3], 16))
+    elif asm_instr[0] == 'ADDI':
+        bin_instr = (OP_ADDI
+                     + dec_str_to_bin(asm_instr[1].replace('$', ''), 5)
+                     + dec_str_to_bin(asm_instr[2].replace('$', ''), 5)
+                     + dec_str_to_bin(asm_instr[3], 16))
     else:
-        instr_bin = '{undefined instruction:' + str(instr_asm) + '}'
+        bin_instr = '{undefined instruction:' + str(asm_instr) + '}'
 
-    return instr_bin
+    return bin_instr
 
 
 # split word for bin instructions
@@ -128,10 +130,28 @@ def format_bin_to_out(instr_bin_str):
 
 
 def main():
-    f = open('rom_raw.txt')
+    # read input file name
+    try:
+        asm_path = sys.argv[1]
+    except IndexError:
+        asm_path = 'rom_raw.txt'
+
+    # read output file name
+    try:
+        dat_path = sys.argv[2]
+    except IndexError:
+        dat_path = asm_path.replace('.txt', '.dat')
+        if dat_path.rfind('.dat') == -1:
+            dat_path += '.dat'
+
+    print('input file path:\t\t' + asm_path)
+    print('output file path:\t\t' + dat_path)
+
+    # read asm file
+    read_file = open(asm_path)
 
     # preprocess
-    asm_instr_list = [i.replace('\r', '').replace('\n', '').replace('\t', ' ') for i in f.readlines()]
+    asm_instr_list = [i.replace('\r', '').replace('\n', '').replace('\t', ' ') for i in read_file.readlines()]
 
     for asm_instr in asm_instr_list:
         if asm_instr == '':
@@ -154,7 +174,13 @@ def main():
     print('%s instruction list:\t' % ('bin' if not _FORMAT_BIN_TO_HEX_ else 'hex'), end='')
     print(out_instr_list)
 
-    f.close()
+    read_file.close()
+
+    # write dat file
+    write_file = open(dat_path, 'w')
+    for i in out_instr_list:
+        write_file.write(i+'\n')
+    write_file.close()
 
 
 if __name__ == '__main__':
